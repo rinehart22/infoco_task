eacc828461ef52cc734ee3782d4faed20bb7df66
rinehart22 <96689631+rinehart22@users.noreply.github.com>
2022-09-03 10:47:31 +0530

    Update urls.py

--- frame/urls.py
+++ frame/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework.documentation import include_docs_urls
 from rest_framework.schemas.coreapi import AutoSchema
 #from rest_framework.schemas.inspectors import AutoSchema
@@ -45,3 +46,4 @@ urlpatterns += [
 
     path('token/verify/', TokenVerifyView.as_view()),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
