bf469dccaa5b3ed73c660ce00ae175978a544d07
rinehart22 <96689631+rinehart22@users.noreply.github.com>
2022-09-03 10:44:58 +0530

    Update models.py

--- fin_infocomtask/models.py
+++ fin_infocomtask/models.py
@@ -9,23 +9,23 @@ class Employee(models.Model):
     age = models.PositiveIntegerField()
     gender=models.CharField(max_length=20)
     phone_no=models.CharField(max_length=20)
-    #address_details=models.
+    address_details=models.CharField(max_length=200)
     h_no=models.CharField(max_length=200)
     street=models.CharField(max_length=20)
     city=models.CharField(max_length=20)
     state=models.CharField(max_length=20)
-    #work_experience = dict
+    work_experience = models.CharField(max_length=200)
     company=models.CharField(max_length=200)
     from_date=models.DateField()
     to_date=models.DateField()
     address=models.CharField(max_length=200)
-    #qualifications= dict
+    qualifications = models.CharField(max_length=200)
     qualification_name=models.CharField(max_length=200)
     percentage=models.FloatField()
-    #projects=
+    projects = models.CharField(max_length=200)
     title=models.CharField(max_length=200)
     description=models.TextField(max_length=200)
-    #photo= base64 
+    photo= models.ImageField(upload_to='media') 
 
     def __str__(self):
        
